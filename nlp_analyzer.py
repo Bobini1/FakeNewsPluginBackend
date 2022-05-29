dbe48efe2cd1cfc5979778bd297c629fb83fdf8d
Tomasz Kalisiak <t.o.m.asz.ka.li.siak.co.uk@gmail.com>
2022-05-29 23:03:37 +0200

    Switch the pickled model to the new one and delete the old one from root

--- nlp_analyzer.py
+++ nlp_analyzer.py
@@ -6,7 +6,7 @@ import NLP
 
 # NLP.generate_models()
 
-model = pickle.load(open('lr_w2v.pkl', 'rb'))
+model = pickle.load(open('NLP/lr_w2v.pkl', 'rb'))
 word2vec_model = pickle.load(open('NLP/word2vec-model.pkl', 'rb'))
 
 
